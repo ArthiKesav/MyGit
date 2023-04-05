e8eccbccad755ae55840cf79860a8c29e1d80c8e
ArthiKesav <arthinirmala16@gmail.com>
2023-04-05 20:30:38 +0530

    Three Methods

---
+++ src/test/java/com/test/A.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class A {
+
+	private void test1() {
+		System.out.println("Test 11");
+	}
+	private void test2() {
+		System.out.println("Test 12");
+	}
+	private void test3() {
+		System.out.println("Test 13");
+	}
+}
